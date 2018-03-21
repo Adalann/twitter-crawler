238369bf403e26c53646a6c5e7a86a59ba03f490
jperret21 <perretjules@gmail.com>
2018-03-21 13:37:12 +0100

    added tweet class

---
+++ src/client/Retweet.java
@@ -0,0 +1,12 @@
+public class Retweet
+{
+    private String id_str;
+    private String text;
+
+    public String toString()
+        {
+            return "ID du Tweet: "+id_str+"text: "+text;
+        }
+
+
+}
