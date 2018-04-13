2c6a51173508ba0dcec404351ae68b075a225267
Théo Martos <theo.martos@hotmail.fr>
2018-04-13 10:11:56 +0200

    Completed classes QuotedTweet & Retweet

--- src/common/QuotedTweet.java
+++ src/common/QuotedTweet.java
@@ -15,6 +15,7 @@ public class QuotedTweet implements Serializable
     public final String id_str;
     public final User user;
     public final String text;
+    public final Entities entities;
     public final String created_at;
     public final int retweet_count;
     public final int quote_count;
@@ -24,6 +25,7 @@ public class QuotedTweet implements Serializable
         this.id_str = "";
         this.user = null;
         this.text = "";
+        this.entities = null;
         this.created_at = "";
         this.retweet_count = 0;
         this.quote_count = 0;
@@ -32,6 +34,7 @@ public class QuotedTweet implements Serializable
     @Override
     public String toString()
     {
-        return "{id=" + id_str + ", user=" + user + ", text=" + text + ", created_at=" + created_at + ", rt=" + retweet_count + ", quoted_count=" + quote_count + "}";
+        return "{id=" + id_str + ", user=" + user + ", text=\"" + text + "\", entities=" + entities + ", created_at=" + created_at + ", rt=" + retweet_count + ", quoted_count=" + quote_count + "}";
     }
 }
+text=\"" + text + "\", entities=" + entities +
